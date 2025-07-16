80bd628f3e7e370d1f206375399603a76532f51c
chanwookK <kan0202@naver.com>
2025-07-17 01:18:33 +0900

    [Init] 프로젝트 초기 셋팅 커밋

---
+++ src/main/java/com/logmate/streaming/LogmateStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.logmate.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogmateStreamingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogmateStreamingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/logmate/streaming/LogmateStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmate.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogmateStreamingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
