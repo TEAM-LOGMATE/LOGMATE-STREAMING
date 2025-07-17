bc3d03a5beb9c45b91ad6bfd3bb351a98f4934cb
chanwookK <kan0202@naver.com>
2025-07-18 02:40:09 +0900

    [ADD] 메인 클래스에 EnableKafka 어노테이션 추가 #1

--- src/main/java/com/logmate/streaming/LogmateStreamingApplication.java
+++ src/main/java/com/logmate/streaming/LogmateStreamingApplication.java
@@ -2,8 +2,10 @@ package com.logmate.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class LogmateStreamingApplication {
 
   public static void main(String[] args) {
