6681b476ca02dfb4bcc7bf46317311accb25c933
chanwookK <kan0202@naver.com>
2025-08-20 20:46:47 +0900

    [Feat] Ai, Kafka 에서 필요한 상수 클래스 추가 및 구현

--- src/main/java/com/logmate/streaming/common/constant/kafka/KafkaConstant.java
+++ src/main/java/com/logmate/streaming/common/constant/kafka/KafkaConstant.java
@@ -1,12 +1,15 @@
 package com.logmate.streaming.common.constant.kafka;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class KafkaConstant {
 
+  @Value("${spring.kafka.consumer.group-id}")
+  public String groupId;
   public final KafkaTopic topic;
   public final KafkaServer server;
 }
