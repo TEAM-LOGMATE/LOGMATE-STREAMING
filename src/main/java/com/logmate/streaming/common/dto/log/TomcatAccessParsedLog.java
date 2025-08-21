53521d8803f547417189c155583cdf8c4bf84b9b
chanwookK <kan0202@naver.com>
2025-08-21 23:24:05 +0900

    [Refactor] TomcatAccessParsedLog 에서 필요 없는 데이터 받지 않아도 되도록 수정

--- src/main/java/com/logmate/streaming/common/dto/log/TomcatAccessParsedLog.java
+++ src/main/java/com/logmate/streaming/common/dto/log/TomcatAccessParsedLog.java
@@ -1,13 +1,18 @@
 package com.logmate.streaming.common.dto.log;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TomcatAccessParsedLog {
-
+  @JsonProperty("formatCorrect")
   private boolean isFormatCorrect;
   private String ip;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
   private LocalDateTime timestamp;
   private String method;
   private String url;
