77e9903bde83d83507797675e8f6157afb5b5a27
chanwookK <kan0202@naver.com>
2025-09-15 03:25:55 +0900

    [Fix] kafka url 주입 버그 수정

--- src/main/java/com/logmate/streaming/common/constant/kafka/KafkaServer.java
+++ src/main/java/com/logmate/streaming/common/constant/kafka/KafkaServer.java
@@ -1,6 +1,5 @@
 package com.logmate.streaming.common.constant.kafka;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +12,6 @@ public class KafkaServer {
   @Value("${kafka.server.port}")
   public String PORT;
 
-  public String URL;
-
-  @PostConstruct
-  public void init() {
-    URL = HOST + ":" + PORT;
-  }
+  @Value("${spring.kafka.bootstrap-servers}")
+  public String bootstrapServers;
 }
