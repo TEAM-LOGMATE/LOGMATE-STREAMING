4bb7fcca950f6a24b6116c19448c754b67b69406
chanwookK <kan0202@naver.com>
2025-09-14 21:07:43 +0900

    [Comment] LogProcessor 주석 추가

--- src/main/java/com/logmate/streaming/processor/LogProcessor.java
+++ src/main/java/com/logmate/streaming/processor/LogProcessor.java
@@ -11,6 +11,10 @@ import reactor.core.publisher.Mono;
  * 모든 Processor는 order 값을 가져 순서를 결정한다.
  * - order 값이 낮을수록 먼저 실행된다.
  * - 동일한 order 값의 Processor들은 병렬로 실행된다.
+ *
+ * 확장 가이드:
+ *  - 새로운 로그 처리 단계를 추가하려면 이 인터페이스를 구현한다.
+ *  - 예: Ai 처리 Processor, DB 저장 Processor 등
  */
 public interface LogProcessor {
   int getOrder();
