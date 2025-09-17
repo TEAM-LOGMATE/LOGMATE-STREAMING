2c87e9f2a1c3e1e1b8cded8642b4e74e312b3ae7
chanwookK <kan0202@naver.com>
2025-09-18 00:34:02 +0900

    [Refactor] SpringbootLog에서 필요 없는 userCode 제거

--- src/main/java/com/logmate/streaming/common/log/SpringBootParsedLog.java
+++ src/main/java/com/logmate/streaming/common/log/SpringBootParsedLog.java
@@ -1,16 +1,20 @@
 package com.logmate.streaming.common.log;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SpringBootParsedLog {
-
+  @JsonProperty("formatCorrect")
   private boolean isFormatCorrect;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
   private LocalDateTime timestamp;
   private String level;
   private String thread;
   private String logger;
   private String message;
-  private String userCode;
 }
