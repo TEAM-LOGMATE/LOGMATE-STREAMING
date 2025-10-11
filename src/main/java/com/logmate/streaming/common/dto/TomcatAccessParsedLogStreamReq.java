7e9409d1736b7183ef7831ba5ee4ddd753ca5ac0
kangchanwook <kan0202@naver.com>
2025-10-12 03:36:02 +0900

    [Fix] DTO 객체에 기본 생성자가 없던 버그 수정

--- src/main/java/com/logmate/streaming/common/dto/TomcatAccessParsedLogStreamReq.java
+++ src/main/java/com/logmate/streaming/common/dto/TomcatAccessParsedLogStreamReq.java
@@ -1,10 +1,14 @@
 package com.logmate.streaming.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TomcatAccessParsedLogStreamReq {
